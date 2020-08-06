3df973fe020ec58c870a2364308bd3d386ca7875
Michael Currin <michael.currin@gmail.com>
2020-08-06 23:17:32 +0200

    test: Update tests

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -21,3 +21,12 @@ async function main() {
 }
 
 main();
+
+var assert = require('assert');
+describe('Array', function () {
+	describe('#indexOf()', function () {
+		it('should return -1 when the value is not present', function () {
+			assert.equal([1, 2, 3].indexOf(1), -1);
+		});
+	});
+});
--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -3,7 +3,6 @@ import * as Mocha from 'mocha';
 import * as glob from 'glob';
 
 export function run(): Promise<void> {
-	// Create the mocha test
 	const mocha = new Mocha({
 		ui: 'tdd'
 	});
