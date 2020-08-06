0307ad731799874777eeac347e6d6b36c306ac2d
Michael Currin <michael.currin@gmail.com>
2020-08-06 22:37:34 +0200

    chore: Rename extension

--- src/extension.ts
+++ src/extension.ts
@@ -7,12 +7,12 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld-sample" is now active!');
+	console.log('Congratulations, your extension "autoCommitMsg-sample" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('extension.autoCommitMsg', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
