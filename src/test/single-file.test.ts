e29d4c51e79119080536c2e3375231d854f112c0
Michael Currin <michael.currin@gmail.com>
2020-08-09 13:42:17 +0200

    feat: Update message.js

--- src/test/single-file.test.ts
+++ src/test/single-file.test.ts
@@ -6,6 +6,8 @@ describe('Generate commit message for a single changed file', function() {
   describe('#one()', function() {
     it('should return the appropriate commit message for a new file', function() {
       assert.equal(one('new file', 'foo.txt'), 'Create foo.txt');
+
+      assert.equal(one('new file', 'bar.js'), 'Create bar.js');
     });
   });
 });
