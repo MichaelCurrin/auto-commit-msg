14981978a466be226ddd75f992dfe64c0308fc40
Michael Currin <michael.currin@gmail.com>
2020-08-09 13:52:41 +0200

    feat: Support action

--- src/message.ts
+++ src/message.ts
@@ -1,3 +1,15 @@
 export function one(action: string, path: string): string {
-  return `Create ${path}`;
+  let msgAction;
+  switch (action) {
+    case 'new file':
+      msgAction = 'Create';
+      break;
+    case 'delete':
+      msgAction = 'Delete';
+      break;
+    default:
+      throw new Error(`Invalid action: ${action}`);
+  }
+
+  return `${msgAction} ${path}`;
 }
--- src/test/single-file.test.ts
+++ src/test/single-file.test.ts
@@ -9,5 +9,11 @@ describe('Generate commit message for a single changed file', function() {
 
       assert.equal(one('new file', 'bar.js'), 'Create bar.js');
     });
+
+    it('should return the appropriate commit message for a deleted file', function() {
+      assert.equal(one('delete', 'foo.txt'), 'Delete foo.txt');
+
+      assert.equal(one('delete', 'bar.js'), 'Delete bar.js');
+    });
   });
 });
