6e76c77394d44d0270cd9e67314766c26e07a9e4
Michael Currin <michael.currin@gmail.com>
2020-08-10 17:36:47 +0200

    feat: Add commands

--- src/gitCommands.ts
+++ src/gitCommands.ts
@@ -10,6 +10,7 @@ export class Git {
     return exec(`git ${command} ${options.join(' ')}`, { cwd });
   }
 
+  // TODO: This can be removed if not used.
   static async status(options: string[] = []) {
     return this.execute(getWorkspaceFolder(), 'status', [
       '-s',
@@ -17,5 +18,56 @@ export class Git {
       '--porcelain',
       ...options
     ]);
+  }
+
+  /** 
+   * Run git diff-index with flags.
+   * 
+   * If the CLI command was an empty string (no changes), then the result would be `[ '' ]`,
+   * so return empty array instead.
+   */
+  private static async diffIndex(
+    options: string[] = []
+  ): Promise<Array<string>> {
+    const {
+      stdout,
+      stderr
+    } = await this.execute(getWorkspaceFolder(), 'diff-index', [
+      '--name-status',
+      ...options,
+      'HEAD'
+    ]);
+
+    if (stderr) {
+      console.debug('stderror for git diff-index command:', stderr);
+    }
+
+    if (stdout.length === 0) {
+      return [];
+    }
+    return stdout.split('\n');
+  }
+
+  /** Summary of diff for staged and unstaged files, excluding untracked. **/
+  static async allChanges() {
+    return this.diffIndex();
+  }
+
+  /** Summary of diff for staged files, excluding untracked. **/
+  static async stagedChanges() {
+    return this.diffIndex([ '--cached' ]);
+  }
+
+  /** Look for diff of staged files otherwise fall back to all files. Always excludes untracked */
+  static async getChanges() {
+    const staged = await this.stagedChanges();
+
+    if (staged.length === 0) {
+      console.debug('No staged changes found, using unstaged');
+      return this.allChanges();
+    }
+
+    console.debug('Found staged changes');
+    return staged;
   }
 }
