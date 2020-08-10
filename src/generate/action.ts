65cb5d3e0ffb44687b6c6768d012c0623e56dd13
Michael Currin <michael.currin@gmail.com>
2020-08-10 10:27:37 +0200

    docs: Add docstrings

--- src/generate/action.ts
+++ src/generate/action.ts
@@ -18,6 +18,11 @@ export function describeAction(key: DescriptionStrings) {
   return ACTION[key];
 }
 
+/**
+ * Extract single action from given X and Y actions.
+ * 
+ * Modified takes preferences over the others. There is no way here to combine update and move.
+ */
 export function lookupAction(x: string, y: string): string {
   // Lookup value from enum dynamically without getting a TS error.
   const actionX = (<any>ACTION)[x];
@@ -26,7 +31,12 @@ export function lookupAction(x: string, y: string): string {
   return actionY === ACTION.M ? actionY : actionX;
 }
 
+/**
+ * Return statement as old path to new path, using just target directory
+ * for new path if the file is not renamed.
+ * 
+ * New path with always be full, ignoring any common base.
+ */
 export function pathToPath(oldPath: string, newPath: string): string {
-  // TODO Check common base dir for move vs rename
   return `${oldPath} to ${newPath}`;
 }
