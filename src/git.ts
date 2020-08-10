d6486444f18cf782e7dda585b72f38a9a9d9093d
Michael Currin <michael.currin@gmail.com>
2020-08-10 12:49:17 +0200

    feat: Add status and remove commands

--- src/git.ts
+++ src/git.ts
@@ -6,27 +6,16 @@ import { getWorkspaceFolder } from './workspace';
 const exec = util.promisify(childProcess.exec);
 
 export class Git {
-  static exists() {
-    return this.execute(getWorkspaceFolder(), '', [ '--version' ]);
-  }
-
-  static async add() {
-    const { stdout: changes } = await this.diff([ '--cached' ]);
-
-    if (changes.length === 0) {
-      return this.execute(getWorkspaceFolder(), 'add', [ `--all` ]);
-    }
-  }
-
-  static async commit(message: string, options: string[]) {
-    return this.execute(getWorkspaceFolder(), 'commit', [ `--message="${message}"`, ...options ]);
-  }
-
   static execute(cwd: string, command?: string, options: string[] = []) {
     return exec(`git ${command} ${options.join(' ')}`, { cwd });
   }
 
-  private static async diff(options: string[] = []) {
-    return this.execute(getWorkspaceFolder(), 'diff', [ ...options ]);
+  static async status(options: string[] = []) {
+    return this.execute(getWorkspaceFolder(), 'status', [
+      '-s',
+      '-uno',
+      '--porcelain',
+      ...options
+    ]);
   }
 }
