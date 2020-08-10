2d224b959425e3d42a32f66a6e7f2b4d71c2ee3a
Michael Currin <michael.currin@gmail.com>
2020-08-10 13:30:15 +0200

    chore: Restore git.d.ts and add license link

--- src/api/git.d.ts
+++ src/api/git.d.ts
@@ -1,14 +1,21 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License.
- * 
- *  This copied from git-prefix with a notice about license.txt but I didn't find one there.
- *--------------------------------------------------------------------------------------------*/
 /**
- * Type definitions around git, allowing use of GitExtension and Repository in extensions.ts script.
+ * Type definitions for VS Code Git Extension.
+ * 
+ * For use extensions.ts script.
+ * 
+ * Based on instructions: 
+ *   https://github.com/microsoft/vscode/tree/master/extensions/git#api
+ * 
+ * The file is copied below as is except for the link pasted in.
  */
 
-import { Uri, Event } from 'vscode';
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. https://github.com/microsoft/vscode/blob/master/LICENSE.txt
+ *--------------------------------------------------------------------------------------------*/
+
+import { Uri, Event, Disposable, ProviderResult } from 'vscode';
+export { ProviderResult } from 'vscode';
 
 export interface Git {
   readonly path: string;
@@ -46,7 +53,10 @@ export interface Commit {
   readonly hash: string;
   readonly message: string;
   readonly parents: string[];
-  readonly authorEmail?: string | undefined;
+  readonly authorDate?: Date;
+  readonly authorName?: string;
+  readonly authorEmail?: string;
+  readonly commitDate?: Date;
 }
 
 export interface Submodule {
@@ -121,6 +131,22 @@ export interface RepositoryUIState {
 export interface LogOptions {
   /** Max number of log entries to retrieve. If not specified, the default is 32. */
   readonly maxEntries?: number;
+  readonly path?: string;
+}
+
+export interface CommitOptions {
+  all?: boolean | 'tracked';
+  amend?: boolean;
+  signoff?: boolean;
+  signCommit?: boolean;
+  empty?: boolean;
+}
+
+export interface BranchQuery {
+  readonly remote?: boolean;
+  readonly pattern?: string;
+  readonly count?: number;
+  readonly contains?: string;
 }
 
 export interface Repository {
@@ -166,6 +192,7 @@ export interface Repository {
   createBranch(name: string, checkout: boolean, ref?: string): Promise<void>;
   deleteBranch(name: string, force?: boolean): Promise<void>;
   getBranch(name: string): Promise<Branch>;
+  getBranches(query: BranchQuery): Promise<Ref[]>;
   setBranchUpstream(name: string, upstream: string): Promise<void>;
 
   getMergeBase(ref1: string, ref2: string): Promise<string>;
@@ -175,6 +202,7 @@ export interface Repository {
 
   addRemote(name: string, url: string): Promise<void>;
   removeRemote(name: string): Promise<void>;
+  renameRemote(name: string, newName: string): Promise<void>;
 
   fetch(remote?: string, ref?: string, depth?: number): Promise<void>;
   pull(unshallow?: boolean): Promise<void>;
@@ -186,13 +214,59 @@ export interface Repository {
 
   blame(path: string): Promise<string>;
   log(options?: LogOptions): Promise<Commit[]>;
+
+  commit(message: string, opts?: CommitOptions): Promise<void>;
 }
 
+export interface RemoteSource {
+  readonly name: string;
+  readonly description?: string;
+  readonly url: string | string[];
+}
+
+export interface RemoteSourceProvider {
+  readonly name: string;
+  readonly icon?: string; // codicon name
+  readonly supportsQuery?: boolean;
+  getRemoteSources(query?: string): ProviderResult<RemoteSource[]>;
+  publishRepository?(repository: Repository): Promise<void>;
+}
+
+export interface Credentials {
+  readonly username: string;
+  readonly password: string;
+}
+
+export interface CredentialsProvider {
+  getCredentials(host: Uri): ProviderResult<Credentials>;
+}
+
+export interface PushErrorHandler {
+  handlePushError(
+    repository: Repository,
+    remote: Remote,
+    refspec: string,
+    error: Error & { gitErrorCode: GitErrorCodes }
+  ): Promise<boolean>;
+}
+
+export type APIState = 'uninitialized' | 'initialized';
+
 export interface API {
+  readonly state: APIState;
+  readonly onDidChangeState: Event<APIState>;
   readonly git: Git;
   readonly repositories: Repository[];
   readonly onDidOpenRepository: Event<Repository>;
   readonly onDidCloseRepository: Event<Repository>;
+
+  toGitUri(uri: Uri, ref: string): Uri;
+  getRepository(uri: Uri): Repository | null;
+  init(root: Uri): Promise<Repository | null>;
+
+  registerRemoteSourceProvider(provider: RemoteSourceProvider): Disposable;
+  registerCredentialsProvider(provider: CredentialsProvider): Disposable;
+  registerPushErrorHandler(handler: PushErrorHandler): Disposable;
 }
 
 export interface GitExtension {
@@ -210,4 +284,42 @@ export interface GitExtension {
 	 * @returns API instance
 	 */
   getAPI(version: 1): API;
+}
+
+export const enum GitErrorCodes {
+  BadConfigFile = 'BadConfigFile',
+  AuthenticationFailed = 'AuthenticationFailed',
+  NoUserNameConfigured = 'NoUserNameConfigured',
+  NoUserEmailConfigured = 'NoUserEmailConfigured',
+  NoRemoteRepositorySpecified = 'NoRemoteRepositorySpecified',
+  NotAGitRepository = 'NotAGitRepository',
+  NotAtRepositoryRoot = 'NotAtRepositoryRoot',
+  Conflict = 'Conflict',
+  StashConflict = 'StashConflict',
+  UnmergedChanges = 'UnmergedChanges',
+  PushRejected = 'PushRejected',
+  RemoteConnectionError = 'RemoteConnectionError',
+  DirtyWorkTree = 'DirtyWorkTree',
+  CantOpenResource = 'CantOpenResource',
+  GitNotFound = 'GitNotFound',
+  CantCreatePipe = 'CantCreatePipe',
+  PermissionDenied = 'PermissionDenied',
+  CantAccessRemote = 'CantAccessRemote',
+  RepositoryNotFound = 'RepositoryNotFound',
+  RepositoryIsLocked = 'RepositoryIsLocked',
+  BranchNotFullyMerged = 'BranchNotFullyMerged',
+  NoRemoteReference = 'NoRemoteReference',
+  InvalidBranchName = 'InvalidBranchName',
+  BranchAlreadyExists = 'BranchAlreadyExists',
+  NoLocalChanges = 'NoLocalChanges',
+  NoStashFound = 'NoStashFound',
+  LocalChangesOverwritten = 'LocalChangesOverwritten',
+  NoUpstreamBranch = 'NoUpstreamBranch',
+  IsInSubmodule = 'IsInSubmodule',
+  WrongCase = 'WrongCase',
+  CantLockRef = 'CantLockRef',
+  CantRebaseMultipleBranches = 'CantRebaseMultipleBranches',
+  PatchDoesNotApply = 'PatchDoesNotApply',
+  NoPathFound = 'NoPathFound',
+  UnknownPath = 'UnknownPath'
 }
