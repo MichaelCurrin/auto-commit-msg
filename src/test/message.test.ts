62cf8689c3ff66fc0b084718a1f10991c4e912bd
Michael Currin <michael.currin@gmail.com>
2020-08-10 21:44:29 +0200

    test: Update test to pass

--- src/test/message.test.ts
+++ src/test/message.test.ts
@@ -5,19 +5,19 @@ import { one } from '../generate/message';
 describe('Generate commit message for a single changed file', function() {
   describe('#one()', function() {
     it('should return the appropriate commit message for a new file', function() {
-      assert.equal(one('A  foo.txt'), 'Create foo.txt');
+      assert.equal(one('A    foo.txt'), 'Create foo.txt');
     });
 
     it('should return the appropriate commit message for a modified file', function() {
-      assert.equal(one(' M foo.txt'), 'Update foo.txt');
+      assert.equal(one('M    foo.txt'), 'Update foo.txt');
     });
 
     it('should return the appropriate commit message for a deleted file', function() {
-      assert.equal(one('D  foo.txt'), 'Delete foo.txt');
+      assert.equal(one('D    foo.txt'), 'Delete foo.txt');
     });
 
     it('should return the appropriate commit message for a renamed file', function() {
-      assert.equal(one('R  foo.txt -> bar.txt'), 'Rename foo.txt to bar.txt');
+      assert.equal(one('R    foo.txt   bar.txt'), 'Rename foo.txt to bar.txt');
     });
 
     // it('should return the appropriate commit message for a moved file of the same name', function() {
