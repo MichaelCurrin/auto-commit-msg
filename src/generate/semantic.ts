e4d6c1a45e8f5ccdd97a2e0a8d5bfc8fcff4c964
michaelcurrin <michael.currin@gmail.com>
2020-08-11 21:14:56 +0200

    feat: Extend semantic.ts

--- src/generate/semantic.ts
+++ src/generate/semantic.ts
@@ -11,26 +11,28 @@ import { splitPath } from './paths';
 // Exclude package* files here for JS since those can be related to packages and sometimes to other metadata.
 const PACKAGE_NAMES = [
     'dev-requirements.txt',
+    'test-requirements.txt',
     'requirements.txt',
     'Gemfile',
     'Gemfile.lock',
     'package-lock.json',
     'yarn.lock'
   ],
-  CONFIG_EXTENSIONS = [ 'yml', 'yaml', 'json', 'toml' ],
+  LICENSE_NAMES = [ 'LICENSE', 'LICENSE.txt' ],
+  // This may be too broad or clash with other areas such as CI or package unless used close to last in the entire flow.
+  CONFIG_EXTENSIONS = [ 'yml', 'yaml', 'json', 'toml', 'ini', 'cfg' ],
   CONFIG_DIRS = [ '.vscode' ],
   CONFIG_NAMES = [
+    'setup.py',
     'package.json',
     '.gitignore',
-    '.eslintrc.js', // TODO add other forms
     '.editorconfig',
-    '.prettierrc',
     'tsconfig.json',
     'tslint.json'
   ],
   BUILD_NAMES = [ 'Dockerfile', 'docker-compose.yml' ],
   CI_DIRS = [ '.circleci', '.github/workflows' ],
-  CI_NAMES = [ 'netlify.toml', 'travis.yml' ];
+  CI_NAMES = [ 'netlify.toml', 'travis.yml', '.vscodeignore' ];
 
 /**
  * Support conventional commit message for a given file path.
@@ -57,22 +59,27 @@ export class Semantic {
 
   isTestRelated(): boolean {
     return (
+      this.dir.includes('test/') ||
       this.name.includes('.test.') ||
       this.name.includes('.spec.') ||
-      this.dir.includes('test/') ||
-      this.name.startsWith('test_')
+      this.name.startsWith('test_') ||
+      this.name === '.coveragerc'
     );
   }
 
   isCIRelated(): boolean {
     return this.dir in CI_DIRS || this.name in CI_NAMES;
   }
-
+  
+  // Broadly match eslint configs https://eslint.org/docs/user-guide/configuring
+  // And prettier configs https://prettier.io/docs/en/configuration.html
   isConfigRelated(): boolean {
     if (
       path.extname(this.name) in CONFIG_EXTENSIONS ||
       this.dir in CONFIG_DIRS ||
-      this.name in CONFIG_NAMES
+      this.name in CONFIG_NAMES ||
+      this.name.includes('.eslintrc') || 
+      this.name.includes('.prettier')
     ) {
       return true;
     }
@@ -85,14 +92,17 @@ export class Semantic {
     }
     return false;
   }
+  
+  isLicenseRelated() {
+    return this.name in LICENSE_NAMES;
+  }
 
   // TODO: Move to enum
   getType(): string {
     if (this.isCIRelated()) {
       return 'ci';
     }
-
-    if (this.name === 'LICENSE' || this.isPackageRelated() || this.isConfigRelated()) {
+    if (this.isLicenseRelated() || this.isPackageRelated() || this.isConfigRelated()) {
       return 'chore';
     }
     if (this.isDocRelated()) {
