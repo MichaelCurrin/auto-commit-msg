75efcbfb36dc222e9ba8b3becc8bfdc6dd4797ab
Michael Currin <michael.currin@gmail.com>
2020-08-16 15:18:20 +0200

    test: Update semantic.test.ts

--- src/test/semantic.test.ts
+++ src/test/semantic.test.ts
@@ -3,12 +3,12 @@ import { Semantic } from '../generate/semantic';
 
 describe('Test semantic commit logic', function() {
   describe('#isDocRelated()', function() {
-    it('Determines README.md is a doc', function() {
+    it('determines README.md is a doc', function() {
       assert.equal(new Semantic('README.md').isDocRelated(), true);
       assert.equal(new Semantic('FEEDME.md').isDocRelated(), false);
     });
 
-    it('Determines docs file is a doc', function() {
+    it('determines docs file is a doc', function() {
       assert.equal(new Semantic('docs/fizz.md').isDocRelated(), true);
       assert.equal(new Semantic('docs/foo.img').isDocRelated(), true);
 
