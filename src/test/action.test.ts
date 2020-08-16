0b36dc7fb8b004605826769319f9cead60325e84
Michael Currin <michael.currin@gmail.com>
2020-08-16 11:57:48 +0200

    test: Update action.test.ts

--- src/test/action.test.ts
+++ src/test/action.test.ts
@@ -4,30 +4,21 @@ import { moveRenamePath } from '../generate/action';
 
 describe('Desribe a file moved or renamed', function() {
   describe('#moveRenamePath()', function() {
-    it('describes a renamed file', function() {
+    it('can describe a renamed file', function() {
       assert.equal(moveRenamePath('foo.txt', 'bar.txt'), 'Rename foo.txt to bar.txt');
       assert.equal(moveRenamePath('buzz/foo.txt', 'buzz/bar.txt'), 'Rename foo.txt to bar.txt');
     });
 
-    it('describes a moved file', function() {
+    it('can describe a moved file', function() {
       assert.equal(moveRenamePath('buzz/foo.txt', 'fizz/foo.txt'), 'Move foo.txt to fizz');
       assert.equal(moveRenamePath('buzz/foo.txt', 'foo.txt'), 'Move foo.txt to repo root');
     });
 
-    it('describes a remamed and moved file', function() {
-      assert.equal(
-        moveRenamePath('foo.txt', 'fizz/bar.txt'),
-        'Move and rename foo.txt to fizz/bar.txt'
-      );
+    it('can describe a remamed and moved file', function() {
+      assert.equal(moveRenamePath('foo.txt', 'fizz/bar.txt'), 'Move and rename foo.txt to fizz/bar.txt');
       // This is a rare case, so don't bother trying to handle it smarter around paths.
-      assert.equal(
-        moveRenamePath('fuzz/foo.txt', 'fizz/bar.txt'),
-        'Move and rename foo.txt to fizz/bar.txt'
-      );
-      assert.equal(
-        moveRenamePath('fizz/foo.txt', 'bar.txt'),
-        'Move and rename foo.txt to bar.txt at repo root'
-      );
+      assert.equal(moveRenamePath('fuzz/foo.txt', 'fizz/bar.txt'), 'Move and rename foo.txt to fizz/bar.txt');
+      assert.equal(moveRenamePath('fizz/foo.txt', 'bar.txt'), 'Move and rename foo.txt to bar.txt at repo root');
     });
   });
 });
