a79ce63ba25dd8b9b8e58950b05edd245ea7e557
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-12-02 15:22:12 +0200

    chore: Add ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,10 +11,21 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   rules: {
-    'semi': [2, "always"],
+    'semi': [
+      2,
+      'always',
+    ],
+    'comma-dangle': [
+      2,
+      'always-multiline',
+    ],
+    'quotes': [
+      2,
+      'single',
+    ],
     '@typescript-eslint/no-unused-vars': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/no-non-null-assertion': 0,
-  }
+  },
 };
