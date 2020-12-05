845b04b2f3ac00b71b64d3f9df09168ffb6d0926
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2020-12-05 10:43:48 +0200

    test: Update parseOutput.test.ts

--- src/test/git/parseOutput.test.ts
+++ src/test/git/parseOutput.test.ts
@@ -2,65 +2,10 @@ import * as assert from 'assert';
 import { parseDiffIndex, parseStatus } from '../../git/parseOutput';
 import { FileChanges } from '../../git/parseOutput.d';
 
-describe('Split `git status` output into components', function () {
-  describe('#parseStatus()', function () {
-    it('should return the appropriate commit message for a new file', function () {
-      const expected: FileChanges = {
-        x: 'A',
-        y: ' ',
-        from: 'foo.txt',
-        to: '',
-      };
-      assert.deepStrictEqual(parseStatus('A  foo.txt'), expected);
-    });
-
-    it('should return the appropriate commit message for a modified file', function () {
-      const expected: FileChanges = {
-        x: ' ',
-        y: 'M',
-        from: 'foo.txt',
-        to: '',
-      };
-      assert.deepStrictEqual(parseStatus(' M foo.txt'), expected);
-    });
-
-    it('should return the appropriate commit message for a deleted file', function () {
-      const expected: FileChanges = {
-        x: 'D',
-        y: ' ',
-        from: 'foo.txt',
-        to: '',
-      };
-      assert.deepStrictEqual(parseStatus('D  foo.txt'), expected);
-    });
-
-    it('should return the appropriate commit message for a renamed file', function () {
-      const expected: FileChanges = {
-        x: 'R',
-        y: ' ',
-        from: 'foo.txt',
-        to: 'bar.txt',
-      };
-      assert.deepStrictEqual(parseStatus('R  foo.txt -> bar.txt'), expected);
-
-      it('should return the appropriate commit message for a moved file', function () {
-        const expected: FileChanges = {
-          x: 'R',
-          y: ' ',
-          from: 'foo.txt',
-          to: 'fizz/foo.txt',
-        };
-        assert.deepStrictEqual(parseStatus('R  foo.txt -> fizz/foo.txt'), expected);
-      });
-    });
-  });
-});
-
 describe('Split `git diff-index` output into components', function () {
   // The 1st column to 2nd looks like constant with and then 2nd to 3rd looks like 6 chars.
   // R100    tslint.json     src/tslint.json
   // R100    vsc-extension-quickstart.md     src/vsc-extension-quickstart.md
-
   describe('#parseDiffIndex()', function () {
     it('should return the appropriate commit message for a new file', function () {
       const expected: FileChanges = {
@@ -70,6 +15,8 @@ describe('Split `git diff-index` output into components', function () {
         to: '',
       };
       assert.deepStrictEqual(parseDiffIndex('A       foo.txt'), expected);
+      assert.deepStrictEqual(parseDiffIndex('A foo.txt'), expected);
+      assert.deepStrictEqual(parseDiffIndex('A\tfoo.txt'), expected);
     });
 
     it('should return the appropriate commit message for a modified file', function () {
@@ -129,6 +76,61 @@ describe('Split `git diff-index` output into components', function () {
           from: 'fizz/foo.txt',
         };
         assert.deepStrictEqual(parseDiffIndex('R096    bar.txt       fizz/foo.txt'), expected);
+      });
+    });
+  });
+});
+
+// Not a core part of the tool anymore, but the code and tests are kept anyway.
+describe('Split `git status` output into components', function () {
+  describe('#parseStatus()', function () {
+    it('should return the appropriate commit message for a new file', function () {
+      const expected: FileChanges = {
+        x: 'A',
+        y: ' ',
+        from: 'foo.txt',
+        to: '',
+      };
+      assert.deepStrictEqual(parseStatus('A  foo.txt'), expected);
+    });
+
+    it('should return the appropriate commit message for a modified file', function () {
+      const expected: FileChanges = {
+        x: ' ',
+        y: 'M',
+        from: 'foo.txt',
+        to: '',
+      };
+      assert.deepStrictEqual(parseStatus(' M foo.txt'), expected);
+    });
+
+    it('should return the appropriate commit message for a deleted file', function () {
+      const expected: FileChanges = {
+        x: 'D',
+        y: ' ',
+        from: 'foo.txt',
+        to: '',
+      };
+      assert.deepStrictEqual(parseStatus('D  foo.txt'), expected);
+    });
+
+    it('should return the appropriate commit message for a renamed file', function () {
+      const expected: FileChanges = {
+        x: 'R',
+        y: ' ',
+        from: 'foo.txt',
+        to: 'bar.txt',
+      };
+      assert.deepStrictEqual(parseStatus('R  foo.txt -> bar.txt'), expected);
+
+      it('should return the appropriate commit message for a moved file', function () {
+        const expected: FileChanges = {
+          x: 'R',
+          y: ' ',
+          from: 'foo.txt',
+          to: 'fizz/foo.txt',
+        };
+        assert.deepStrictEqual(parseStatus('R  foo.txt -> fizz/foo.txt'), expected);
       });
     });
   });
