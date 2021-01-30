159c6d3992aa3b04c8150d1048a7cdd6657e3157
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-01-30 18:49:07 +0200

    style: Update formating of ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,30 +2,30 @@
 // eslint-disable-next-line no-undef
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   plugins: [
-    '@typescript-eslint',
+    "@typescript-eslint",
   ],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   rules: {
-    'semi': [
+    "semi": [
       2,
-      'always',
+      "always",
     ],
-    'comma-dangle': [
+    "comma-dangle": [
       2,
-      'always-multiline',
+      "always-multiline",
     ],
-    'quotes': [
+    "quotes": [
       2,
-      'double',
+      "double",
     ],
-    '@typescript-eslint/no-unused-vars': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/no-non-null-assertion': 0,
+    "@typescript-eslint/no-unused-vars": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+    "@typescript-eslint/no-non-null-assertion": 0,
   },
 };
