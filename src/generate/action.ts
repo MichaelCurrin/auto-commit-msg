9ae80d1cef792d05cee024e0632ba1d7fed0f750
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-14 23:00:48 +0200

    docs: Update action.ts

--- src/generate/action.ts
+++ src/generate/action.ts
@@ -1,5 +1,11 @@
 /**
+ * Action module.
+ *
  * Phrase commit changes in words.
+ *
+ * This follows the git style of using `x` and `y`. See this project's docs for more info. Note that
+ * `first` and `second` are not appropriate names as `git status` and `git diff-index` behave
+ * differently.
  */
 import { ACTION, ROOT } from "../lib/constants";
 import { splitPath } from "../lib/paths";
@@ -9,11 +15,9 @@ type ActionKeys = keyof typeof ACTION;
 /**
  * Extract single action from given X and Y actions.
  *
- * NOT USED.
+ * NOT USED. But keep for future use with git status short output.
  *
- * This works for git status short output.
- *
- * "Modified" takes preferences over the others. There is no way here to combine update and move.
+ * "Modified" must take preference over the others. There is no way here to combine update and move.
  */
 export function lookupStatusAction(x: string, y: string): string {
   if (ACTION[y as ActionKeys] === ACTION.M) {
@@ -22,6 +26,9 @@ export function lookupStatusAction(x: string, y: string): string {
   return ACTION[x as ActionKeys];
 }
 
+/**
+ * Lookup the action for a given key (single character).
+ */
 export function lookupDiffIndexAction(x: string) {
   return ACTION[x as ActionKeys];
 }
