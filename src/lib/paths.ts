e08bb0679dc2d4b15424b584b93e3ae951f65e50
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-14 23:54:03 +0200

    docs

--- src/lib/paths.ts
+++ src/lib/paths.ts
@@ -10,15 +10,15 @@ import { SplitPathResult } from "./paths.d";
 /**
  * Get metadata for a given path.
  *
- * This is done in one function call to save having to do separate calls or having to the builtin
+ * This is done in one function call to save having to do separate calls or having to the built-in
  * string methods all over the project.
  *
  * Info is derived based on the input value as string, not whether the path to a file that exists or
  * not.
  *
- * Note that path.extname is already smart enough to detect only the last extension if there are
- * multiple dots as see extension as empty string if it is '.filename'. Note that extension has a
- * dot.
+ * Note that `path.extname` is already smart enough to detect only the last extension if there are
+ * multiple dots as see extension as empty string if it is '.filename'. Note that extension will
+ * have a dot.
  */
 export function splitPath(filePath: string): SplitPathResult {
   const dir = path.dirname(filePath),
