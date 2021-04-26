f50daa9300d2bc3b1f043f82ee43ddc1f4273169
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-26 22:53:46 +0200

    docs: Update reset_sandbox.sh

--- bin/reset_sandbox.sh
+++ bin/reset_sandbox.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+# Set up the sandbox directly.
 set -e
 
 if [ -d sandbox ]; then
-    rm -rf sandbox
+  rm -rf sandbox
 fi
 
 git init sandbox --quiet
 cd sandbox
+
+# Create a commit, so that the extension can run diff-index, which requires an initial commit.
 echo "# Sandbox\n" >'README.md'
 git add -A
 git commit -m 'Initial commit in sandbox'
