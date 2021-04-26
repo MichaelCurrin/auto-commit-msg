f71818ea1edc7ba970c244b304b0a03c66fe472a
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-26 20:14:31 +0200

    feat: Create conventionalCommit.ts

---
+++ src/generate/conventionalCommit.ts
@@ -0,0 +1,44 @@
+/**
+ * Conventional commit module.
+ *
+ * Categorize changes of a path using the Conventional Commit standard like `fix`.
+ *
+ * Here all as functions. But rewriting all as classes might be lower lift and okay.
+* Maybe first pass it just rewriting class.method calls in tests (as a new test file) as function
+calls with data.
+ */
+
+import { CONVENTIONAL_TYPE } from "../src/lib/constants";
+
+const CI_CRITERIA = {
+  dirs: {
+    // includes: [],
+    // startsWith: [],
+    equals: [
+      ".circleci",
+      ".github/workflows",
+    ],
+  },
+  // extensions: {
+  //   equals: [],
+  // },
+  names: {
+    includes: [
+      "netlify.toml",
+      "travis.yml",
+      "tox.ini",
+    ],
+    // startsWith: [],
+    // equals: [],
+  },
+};
+
+function related(criteria) {
+  return true;
+}
+
+function main() {
+  if (related(CI_CRITERIA) === true) {
+    return CONVENTIONAL_TYPE.CI;
+  }
+}
