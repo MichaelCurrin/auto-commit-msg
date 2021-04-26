66573e747fe51d29dc5734a253c688396d84fee7
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-26 22:30:22 +0200

    test: Update prepareCommitMsg.test.ts

--- src/test/prepareCommitMsg.test.ts
+++ src/test/prepareCommitMsg.test.ts
@@ -4,13 +4,14 @@
  * Check creation of commit message.
  */
 import * as assert from "assert";
+import { CONVENTIONAL_TYPE } from "../lib/constants";
 import { generateMsgFromChanges } from "../prepareCommitMsg";
 
 describe("Prepare commit message", function () {
   describe("#generateMsgFromChanges()", function () {
     it("handles a single file correctly", function () {
       const expected = {
-        prefix: "feat",
+        prefix: CONVENTIONAL_TYPE.FEAT,
         fileChangeMsg: "Create baz.txt",
       };
 
@@ -20,7 +21,7 @@ describe("Prepare commit message", function () {
 
     it("handles 2 files with the same action correctly", function () {
       const expected = {
-        prefix: "",
+        prefix: CONVENTIONAL_TYPE.UNKNOWN,
         fileChangeMsg: "Create baz.txt and bar.js",
       };
 
@@ -30,7 +31,7 @@ describe("Prepare commit message", function () {
 
     it("handles 2 files with a different action correctly", function () {
       const expected = {
-        prefix: "",
+        prefix: CONVENTIONAL_TYPE.UNKNOWN,
         fileChangeMsg: "Various changes to baz.txt and bar.js",
       };
 
