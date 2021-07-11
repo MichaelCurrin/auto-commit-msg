604456e0d7350761145524602feb2103cecc2a31
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-11 22:39:59 +0200

    style: format

--- src/test/generate/count.test.ts
+++ src/test/generate/count.test.ts
@@ -3,80 +3,74 @@
  */
 
 import * as assert from "assert";
-import { count } from '../../generate/count';
+import { count } from "../../generate/count";
 import { FileChanges } from "../../git/parseOutput.d";
 import { ACTION } from "../../lib/constants";
 
-describe('count', () => {
-  describe('should return the correct action and count for one file', function () {
-    it('should handle a created file', function () {
+describe("count", () => {
+  describe("should return the correct action and count for one file", function () {
+    it("should handle a created file", function () {
       const changes: FileChanges[] = [
         {
           x: ACTION.A,
           y: " ",
           from: "foo.txt",
-          to: ""
-        }
-      ]
+          to: "",
+        },
+      ];
       const expected = {
-        create: 1
-      }
+        create: 1,
+      };
 
-      assert.deepStrictEqual(
-        count(changes), expected)
-    })
+      assert.deepStrictEqual(count(changes), expected);
+    });
 
-    it('should handle an updated file', function () {
+    it("should handle an updated file", function () {
       const changes: FileChanges[] = [
         {
           x: ACTION.M,
           y: " ",
           from: "foo.txt",
-          to: ""
-        }
-      ]
+          to: "",
+        },
+      ];
       const expected = {
-        update: 1
-      }
+        update: 1,
+      };
 
-      assert.deepStrictEqual(
-        count(changes), expected)
-    })
+      assert.deepStrictEqual(count(changes), expected);
+    });
 
-    it('should handle a deleted file', function () {
+    it("should handle a deleted file", function () {
       const changes: FileChanges[] = [
         {
           x: ACTION.D,
           y: " ",
           from: "foo.txt",
-          to: ""
-        }
-      ]
+          to: "",
+        },
+      ];
       const expected = {
-        delete: 1
-      }
+        delete: 1,
+      };
 
-      assert.deepStrictEqual(
-        count(changes), expected)
-    })
+      assert.deepStrictEqual(count(changes), expected);
+    });
 
-    it('should handle a renamed file', function () {
+    it("should handle a renamed file", function () {
       const changes: FileChanges[] = [
         {
           x: ACTION.R,
           y: " ",
           from: "foo.txt",
-          to: "bar.txt"
-        }
-      ]
+          to: "bar.txt",
+        },
+      ];
       const expected = {
-        rename: 1
-      }
+        rename: 1,
+      };
 
-      assert.deepStrictEqual(
-        count(changes), expected)
-    })
+      assert.deepStrictEqual(count(changes), expected);
+    });
   });
-
-
-})
+});
