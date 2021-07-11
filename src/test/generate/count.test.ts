636f8d14155275c8dd5bd4f29f52da9128b4f6b4
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-11 22:21:29 +0200

    test: add cases

--- src/test/generate/count.test.ts
+++ src/test/generate/count.test.ts
@@ -8,20 +8,58 @@ import { FileChanges } from "../../git/parseOutput.d";
 import { ACTION } from "../../lib/constants";
 
 describe('count', () => {
-  it('should return the action and count one file', function () {
-    const changes: FileChanges[] = [
-      {
-        x: ACTION.A,
-        y: " ",
-        from: "foo",
-        to: ""
+  describe('should return the correct action and count for one file', function () {
+    it('should handle a created file', function () {
+      const changes: FileChanges[] = [
+        {
+          x: ACTION.A,
+          y: " ",
+          from: "foo",
+          to: ""
+        }
+      ]
+      const expected = {
+        create: 1
       }
-    ]
-    const expected = {
-      create: 1
-    }
 
-    assert.deepStrictEqual(
-      count(changes), expected)
+      assert.deepStrictEqual(
+        count(changes), expected)
+    })
+
+    it('should handle an updated file', function () {
+      const changes: FileChanges[] = [
+        {
+          x: ACTION.M,
+          y: " ",
+          from: "foo",
+          to: ""
+        }
+      ]
+      const expected = {
+        update: 1
+      }
+
+      assert.deepStrictEqual(
+        count(changes), expected)
+    })
+
+    it('should handle a deleted file', function () {
+      const changes: FileChanges[] = [
+        {
+          x: ACTION.D,
+          y: " ",
+          from: "foo",
+          to: ""
+        }
+      ]
+      const expected = {
+        delete: 1
+      }
+
+      assert.deepStrictEqual(
+        count(changes), expected)
+    })
   });
+
+
 })
