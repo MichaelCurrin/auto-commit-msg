a98b860c876f6d73f87d7b015dd69f751bec0e33
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-12 20:12:10 +0200

    docs: update count.ts

--- src/generate/count.ts
+++ src/generate/count.ts
@@ -4,8 +4,8 @@
  * Rather than dealing with individual file names, this module deals with grouping files by actions
  * and showing a count for each.
  *
- * e.g. 'create 5 files'
- * e.g. 'update 3 files in foo'
+ * e.g. 'create 5 files' (in different directories).
+ * e.g. 'update 3 files in foo' (highest common directory).
  * e.g. 'update 16 files and delete 2 files'
  */
 
