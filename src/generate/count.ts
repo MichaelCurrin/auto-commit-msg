3b979ed3b874f4c0031b4b1a1ca3031020e041d0
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-12 20:07:26 +0200

    docs: update count.ts

--- src/generate/count.ts
+++ src/generate/count.ts
@@ -11,6 +11,10 @@
 
 import { FileChanges } from "../git/parseOutput.d";
 
+// Allowed keys are strictly mean to be from ACTION constant, plus also 'rename' (Git considers
+// rename and move the same thing but it is useful to differeniate here). Keep it flexible - don't
+// bother to validate the key here as this is used internally and from the user nd tests can handle
+// it.
 type CountResult = { [key: string]: number };
 
 export function count(changes: FileChanges[]) {
