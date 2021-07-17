e65e91bf7a1c69f85b11c9a0bc712bc090e42fc9
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-17 17:04:48 +0200

    Update src/generate/count.d.ts

--- src/generate/count.d.ts
+++ src/generate/count.d.ts
@@ -1,4 +1,4 @@
-// Allowed keys are strictly mean to be from ACTION constant, plus also 'rename' (Git considers
+// Allowed keys are strictly meant to be from ACTION constant, plus also 'rename' (Git considers
 // rename and move the same thing but it is useful to differeniate here). Keep it flexible - don't
 // bother to validate the key here as this is used internally and from the user nd tests can handle
 // it.
