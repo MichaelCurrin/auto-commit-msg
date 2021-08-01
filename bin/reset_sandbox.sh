0b98b77290905d8a26f8aebfe28327a6f7b663a8
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-08-01 14:33:57 +0200

    update reset_sandbox.sh

--- bin/reset_sandbox.sh
+++ bin/reset_sandbox.sh
@@ -4,7 +4,7 @@ set -e
 
 DIR='sandbox'
 
-if [ -d "$DIR" ]; then
+if [[ -d "$DIR" ]]; then
   rm -rf "$DIR"
 fi
 
@@ -13,5 +13,21 @@ cd "$DIR"
 
 # Create a commit, so that the extension can run diff-index, which requires an initial commit.
 echo "# Sandbox\n" >'README.md'
+echo 'console.log("Hello, Foo!");' >foo.js
+echo 'console.log("Hello, Fizz!");' >fizz.js
+echo 'console.log("Hello, Buzz!");' >buzz.js
+echo 'console.log("Hello, Bazz!");' >bazz.js
 git add -A
 git commit -m "Initial commit in $DIR"
+
+# Modify.
+echo 'console.log("Hello, Foobar!");' >>foo.js
+# Add.
+echo 'console.log("Hello, Bar!");' >bar.js
+# Rename
+mv fizz.js fizzy.js
+# Move
+mkdir -p my_subdir
+mv bazz.js my_subdir
+# Delete.
+rm buzz.js
