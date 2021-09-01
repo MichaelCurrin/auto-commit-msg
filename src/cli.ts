721c4ed85300c411e8e5d51a3ced738bd46299a4
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-09-01 11:22:46 +0200

    docs: update cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,13 @@
 /**
  * CLI module.
  *
- * This is the entry-point for the Auto Commit Message tool when running as an STANDALONE
- * command-line script.
+ * This is the entry-point for the Auto Commit Message tool when running as a STANDALONE
+ * command-line script i.e. without VS Code.
  *
- * This script does NOT interact with VS Code or any Git commands. It just receives text as an
- * argument and prints output to `stdout` for use in a hook flow. Or `stderr` in the case of a
- * message not appropriate for a commit message.
+ * This script does NOT interact with VS Code or the Git.
+ *
+ * It simply receives text as an argument and prints output to `stdout` for use in a hook flow. Or
+ * `stderr`, in the case of a message not appropriate for a commit message.
  *
  * See `shell/README.md` docs.
  */
