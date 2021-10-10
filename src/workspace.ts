eef2ea0314da45692ce82cc25d778568b55422b9
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-10-10 09:58:49 +0200

    refactor

--- src/workspace.ts
+++ src/workspace.ts
@@ -2,7 +2,6 @@ import { workspace } from "vscode";
 
 export function getWorkspaceFolder(): string {
   const { workspaceFolders } = workspace;
-  console.log({ workspaceFolders });
 
   return workspaceFolders ? workspaceFolders[0].uri.fsPath : "";
 }
