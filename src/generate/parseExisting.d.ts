98f999ae13a962c59aac3dbd04d7aaf12ad09a03
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-10-22 10:58:32 +0200

    docs: update parseExisting.d.ts

--- src/generate/parseExisting.d.ts
+++ src/generate/parseExisting.d.ts
@@ -6,7 +6,8 @@
  *
  * The `typePrefix` here is kept as a string type for simplicity and not having
  * to convert to `CONVENTIONAL_TYPE` too early in process where there isn't a
- * benefit.
+ * benefit. Also, when this is used to parse an existing message, there is no
+ * guarantee that the user's type prefix is one of the expected types.
  *
  * See also the `ConvCommitMsg` type.
  */
