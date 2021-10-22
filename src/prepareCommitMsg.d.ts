298b7720e4dca46dbc21b1e30248688a805ad032
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-10-22 10:11:06 +0200

    docs: update prepareCommitMsg.d.ts

--- src/prepareCommitMsg.d.ts
+++ src/prepareCommitMsg.d.ts
@@ -1,5 +1,11 @@
 import { CONVENTIONAL_TYPE } from "./lib/constants";
 
+/**
+ * Components of the Conventional Commit Message standard.
+ *
+ * For simplicity in this project, the `scope` is ignored and no body or footer
+ * messages are used.
+ */
 export type ConvCommitMsg = {
   typePrefix: CONVENTIONAL_TYPE;
   description: string;
