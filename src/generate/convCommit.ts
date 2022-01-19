8e9904f8c9b1bd88e35a33577d81eacd8f7c0cf0
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2022-01-19 10:12:47 +0200

    refactor: create module of conv commit constants

--- src/generate/convCommit.ts
+++ src/generate/convCommit.ts
@@ -6,230 +6,18 @@
  */
 import { ACTION, CONVENTIONAL_TYPE } from "../lib/constants";
 import { splitPath } from "../lib/paths";
-
-// Package-related can be for 'build'.
-const PACKAGE_DIRS = [
-  // Rust
-  ".cargo",
-],
-  PACKAGE_NAMES = [
-    // Python
-    "requirements.txt",
-    "requirements-dev.txt",
-    "dev-requirements.txt",
-    "requirements-test.txt",
-    "test-requirements.txt",
-
-    "Pipfile",
-    "Pipefile.lock",
-
-    // Make Python package installable and manage external packages.
-    "pyproject.toml",
-    "setup.py",
-
-    // Ruby
-    "Gemfile",
-    "Gemfile.lock",
-
-    // NPM (Exclude package.json since it can be used for metadata and not
-    // package always changes.)
-    "package-lock.json",
-    "shrinkwrap.json",
-    "yarn.lock",
-
-    // PHP
-    "composer.json",
-    "composer.lock",
-
-    // Go
-    "go.mod",
-    "go.sum",
-
-    // Rust
-    "Cargo.toml",
-    "Cargo.lock",
-
-    // Deno
-    "deps.ts",
-    "test_deps.ts",
-    "dev_deps.ts",
-    "import_map.json",
-  ];
-
-// Build system (scripts, configurations or tools) and package dependencies.
-const BUILD_NAMES = [
-  // Docker
-  ".dockerignore",
-  "Dockerfile",
-  "docker-compose.yml",
-
-  // Make
-  "GNUmakefile",
-  "makefile",
-  "Makefile",
-
-  // Ruby Rake - based on rake CLI message.
-  "rakefile",
-  "Rakefile",
-  "rakefile.rb",
-  "Rakefile.rb",
-
-  "package.json", // Not necessarily package-related but always build-related.
-
-  // Java
-  "gradlew",
-  "grailsw",
-  "micronaut-cli.yml",
-],
-  BUILD_EXTENSIONS = [
-    // Ruby installation
-    ".gemspec",
-
-    // Windows
-    ".bat",
-
-    // Java
-    ".gradle",
-  ];
-
-// This may be too broad or clash with other areas such as CI or package, unless
-// used close to LAST in the entire flow.
-// Note also that prettier and ESLint configs with various extensions are
-// handled in isConfigRelated so don't have to be listed explictly. Though those
-// strings should be moved out of the function.
-const CONFIG_EXTENSIONS = [".yml", ".yaml", ".json", ".toml", ".ini", ".cfg"],
-  CONFIG_DIRS = [".vscode"],
-  CONFIG_NAMES = [
-    // Node
-    ".npmignore",
-    ".npmrc",
-
-    // Git
-    ".gitignore",
-
-    // EditorConfig
-    ".editorconfig",
-
-    // Python
-    ".pylintrc",
-    "setup.cfg",
-
-    // Node
-    ".browserslistrc",
-    "browserslist",
-    "commitlint.config.js",
-
-    // TypeScript
-    "tsconfig.json",
-    "tslint.json",
-
-    // Dotenv
-    ".env",
-    ".env.dev",
-    ".env.prod",
-    ".env.tempate",
-  ];
-
-const CI_DIRS = [".circleci", ".github/workflows"],
-  CI_NAMES = [
-    "netlify.toml",
-    "travis.yml",
-    "tox.ini",
-
-    ".vscodeignore",
-
-    "codecov.yml",
-    ".codecov.yml",
-    ".codeclimate.yml",
-
-    // Zeit
-    "now.json",
-    ".nowignore",
-    "vercel.json",
-    ".vercelignore",
-  ];
-
-// This can be useful for multi-file changes e.g. "Create 5 scripts"
-// It might be easier to leave out this list and assume everything is a script
-// unless it is a doc, markdown file or config.
-const SCRIPT_EXTENSIONS = [
-  ".html",
-
-  ".css",
-  ".less",
-  ".scss",
-
-  ".js",
-  ".jsx",
-  ".ts",
-  ".tsx",
-  ".mjs",
-
-  ".py",
-
-  ".rb",
-
-  ".java",
-  ".jar",
-
-  ".c",
-  ".h",
-
-  ".rs",
-
-  ".go",
-
-  ".php",
-],
-  // For "Update 5 shell scripts"
-  SHELL_SCRIPT_EXTENSION = ".sh";
-
-const LICENSE_NAMES = [
-  "LICENSE",
-  "LICENSE.txt",
-  "License.txt",
-  "LICENSE-source",
-];
-
-// TODO: Use.
-const DOCS_DIRS = [
-  "docs",
-  ".github/ISSUE_TEMPLATE",
-  ".github/PULL_REQUEST_TEMPLATE",
-];
-// Anything in `/docs` will be covered already so this is for the root and any
-// subdirectories. Don't worry about .rst as those are already cover as always
-// docs. While `.md` could be content for a static site or docs site.
-const DOC_NAMES = [
-  "README",
-  "README.md",
-  "README.txt",
-
-  "installation.md",
-  "usage.md",
-  "development.md",
-  "deploy.md",
-
-  // GitHub docs.
-  "SECURITY.md",
-  "CONTRIBUTING.md",
-  "CHANGELOG.md",
-  "RELEASES.md",
-  "FUNDING.md",
-  "PULL_REQUEST_TEMPLATE.md",
-  "ISSUE_TEMPLATE.md",
-  "CODE_OF_CONDUCT.md",
-
-  "MAINTAINERS.txt",
-  "CODEOWNERS",
-
-  // Images.
-  "sample.png",
-  "sample-1.png",
-  "sample-2.png",
-  "screenshot.png",
-  "preview.png",
-].map(name => name.toLowerCase());
+import {
+  BUILD_EXTENSIONS,
+  BUILD_NAMES,
+  CI_DIRS,
+  CI_NAMES,
+  CONFIG_DIRS,
+  CONFIG_EXTENSIONS,
+  CONFIG_NAMES,
+  DOC_NAMES,
+  LICENSE_NAMES,
+  PACKAGE_NAMES
+} from "./convCommitConstants";
 
 /**
  * Evaluate conventional commit prefix for a given file.
@@ -266,10 +54,10 @@ export class ConventionalCommit {
   /**
    * Check if doc-related.
    *
-   * Return true for `.rst`, README files and anything in the docs directory.
+   * Return true for `.rst`, README files, and anything in the docs directory.
    *
-   * TODO: For static sites, not all .md files are docs but that could be configured with a global
-   * flag. Or recognize Jekyll config.
+   * TODO: For static sites, not all `.md` files are docs but that could be
+   * configured with a global flag. Or recognize presence of Jekyll config file.
    */
   isDocsRelated(): boolean {
     if (this.extension === ".rst") {
@@ -297,14 +85,15 @@ export class ConventionalCommit {
   }
 
   isCIRelated(): boolean {
-    // Assume flat structure and don't check for nesting in subdirs of CI_DIRS.
+    // Assume flat structure and don't check for nesting in subdirs of
+    // `CI_DIRS`.
     return CI_DIRS.includes(this.dirPath) || CI_NAMES.includes(this.name);
   }
 
-  // Broadly match eslint configs with any extension e.g. .json or .yml
+  // Broadly match eslint configs with any extension e.g. `.json` or `.yml`.
   // See https://eslint.org/docs/user-guide/configuring
   // Same for prettier configs https://prettier.io/docs/en/configuration.html
-  // And tslint* as JSON or YAML and webpack*.
+  // And `tslint*` as JSON or YAML and `webpack*`.
   // See https://github.com/vscode-icons/vscode-icons/blob/master/src/iconsManifest/supportedExtensions.ts
   isConfigRelated(): boolean {
     return (
---
+++ src/generate/convCommitConstants.ts
@@ -0,0 +1,236 @@
+/**
+ * Conventional Commit constants module.
+ */
+
+// Package-related can be for 'build'.
+export const PACKAGE_DIRS = [
+  // Rust
+  ".cargo",
+];
+
+export const PACKAGE_NAMES = [
+  // Python
+  "requirements.txt",
+  "requirements-dev.txt",
+  "dev-requirements.txt",
+  "requirements-test.txt",
+  "test-requirements.txt",
+
+  "Pipfile",
+  "Pipefile.lock",
+
+  // Make Python package installable and manage external packages.
+  "pyproject.toml",
+  "setup.py",
+
+  // Ruby
+  "Gemfile",
+  "Gemfile.lock",
+
+  // NPM (Exclude package.json since it can be used for metadata and not
+  // package always changes.)
+  "package-lock.json",
+  "shrinkwrap.json",
+  "yarn.lock",
+
+  // PHP
+  "composer.json",
+  "composer.lock",
+
+  // Go
+  "go.mod",
+  "go.sum",
+
+  // Rust
+  "Cargo.toml",
+  "Cargo.lock",
+
+  // Deno
+  "deps.ts",
+  "test_deps.ts",
+  "dev_deps.ts",
+  "import_map.json",
+];
+
+// Build system (scripts, configurations or tools) and package dependencies.
+export const BUILD_NAMES = [
+  // Docker
+  ".dockerignore",
+  "Dockerfile",
+  "docker-compose.yml",
+
+  // Make
+  "GNUmakefile",
+  "makefile",
+  "Makefile",
+
+  // Ruby Rake - based on rake CLI message.
+  "rakefile",
+  "Rakefile",
+  "rakefile.rb",
+  "Rakefile.rb",
+
+  "package.json", // Not necessarily package-related but always build-related.
+
+  // Java
+  "gradlew",
+  "grailsw",
+  "micronaut-cli.yml",
+];
+export const BUILD_EXTENSIONS = [
+  // Ruby installation
+  ".gemspec",
+
+  // Windows
+  ".bat",
+
+  // Java
+  ".gradle",
+];
+
+// This may be too broad or clash with other areas such as CI or package, unless
+// used close to LAST in the entire flow.
+// Note also that prettier and ESLint configs with various extensions are
+// handled in isConfigRelated so don't have to be listed explictly. Though those
+// strings should be moved out of the function.
+export const CONFIG_EXTENSIONS = [
+  ".yml",
+  ".yaml",
+  ".json",
+  ".toml",
+  ".ini",
+  ".cfg",
+];
+export const CONFIG_DIRS = [".vscode"];
+export const CONFIG_NAMES = [
+  // Node
+  ".npmignore",
+  ".npmrc",
+
+  // Git
+  ".gitignore",
+
+  // EditorConfig
+  ".editorconfig",
+
+  // Python
+  ".pylintrc",
+  "setup.cfg",
+
+  // Node
+  ".browserslistrc",
+  "browserslist",
+  "commitlint.config.js",
+
+  // TypeScript
+  "tsconfig.json",
+  "tslint.json",
+
+  // Dotenv
+  ".env",
+  ".env.dev",
+  ".env.prod",
+  ".env.tempate",
+];
+
+export const CI_DIRS = [".circleci", ".github/workflows"];
+export const CI_NAMES = [
+  "netlify.toml",
+  "travis.yml",
+  "tox.ini",
+
+  ".vscodeignore",
+
+  "codecov.yml",
+  ".codecov.yml",
+  ".codeclimate.yml",
+
+  // Zeit
+  "now.json",
+  ".nowignore",
+  "vercel.json",
+  ".vercelignore",
+];
+
+// This can be useful for multi-file changes e.g. "Create 5 scripts"
+// It might be easier to leave out this list and assume everything is a script
+// unless it is a doc, markdown file or config.
+export const SCRIPT_EXTENSIONS = [
+  ".html",
+
+  ".css",
+  ".less",
+  ".scss",
+
+  ".js",
+  ".jsx",
+  ".ts",
+  ".tsx",
+  ".mjs",
+
+  ".py",
+
+  ".rb",
+
+  ".java",
+  ".jar",
+
+  ".c",
+  ".h",
+
+  ".rs",
+
+  ".go",
+
+  ".php",
+];
+// For "Update 5 shell scripts"
+export const SHELL_SCRIPT_EXTENSION = ".sh";
+
+export const LICENSE_NAMES = [
+  "LICENSE",
+  "LICENSE.txt",
+  "License.txt",
+  "LICENSE-source",
+];
+
+// TODO: Use.
+export const DOCS_DIRS = [
+  "docs",
+  ".github/ISSUE_TEMPLATE",
+  ".github/PULL_REQUEST_TEMPLATE",
+];
+
+// Anything in `/docs` will be covered already so this is for the root and any
+// subdirectories. Don't worry about .rst as those are already cover as always
+// docs. While `.md` could be content for a static site or docs site.
+export const DOC_NAMES = [
+  "README",
+  "README.md",
+  "README.txt",
+
+  "installation.md",
+  "usage.md",
+  "development.md",
+  "deploy.md",
+
+  // GitHub docs.
+  "SECURITY.md",
+  "CONTRIBUTING.md",
+  "CHANGELOG.md",
+  "RELEASES.md",
+  "FUNDING.md",
+  "PULL_REQUEST_TEMPLATE.md",
+  "ISSUE_TEMPLATE.md",
+  "CODE_OF_CONDUCT.md",
+
+  "MAINTAINERS.txt",
+  "CODEOWNERS",
+
+  // Images.
+  "sample.png",
+  "sample-1.png",
+  "sample-2.png",
+  "screenshot.png",
+  "preview.png",
+].map(name => name.toLowerCase());
