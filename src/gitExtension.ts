c29ef86920f8b059e7078f87ab818af70777fb7e
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2022-07-26 16:02:03 +0200

    docs: update gitExtension.ts

--- src/gitExtension.ts
+++ src/gitExtension.ts
@@ -6,7 +6,7 @@
  * This module takes care of the high-level flow of the extension, after a repo
  * is selected in extension.ts module.
  *
- * This module handles reading git output, processes it with the logic in the
+ * This module handles reading Git output, processes it with the logic in the
  * generate module and then finally sets it in the UI message box.
  */
 import * as vscode from "vscode";
@@ -14,18 +14,13 @@ import { GitExtension, Repository } from "./api/git";
 
 /**
  * Fetch the commit message in the Git Extension.
- *
- * This could useful when doing conventional commits, as the initial 'feat' or
- * 'feat: ' portion or similar can be kept as a prefix while the generated
- * message added on. Or if left out, it can be generated if possible such as for
- * 'chore' or 'docs'.
  */
 export function getCommitMsg(repository: Repository): string {
   return repository.inputBox.value;
 }
 
 /**
- * Set the commit message in the Git Extension.
+ * Set the commit message in the Git Extension pane's input.
  */
 export function setCommitMsg(repository: Repository, msg: string) {
   repository.inputBox.value = msg;
