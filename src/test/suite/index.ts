22ca36949a72e079bb45031fc84c5d109f499e89
Aleksei Ivanov <contact@aleksei.dev>
2023-03-01 14:57:25 +0300

    test: update src/test/suite/index.ts

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -3,35 +3,31 @@ import * as Mocha from "mocha";
 import * as glob from "glob";
 
 export function run(): Promise<void> {
-  // Create the mocha test
   const mocha = new Mocha({
     ui: "tdd",
   });
-  // mocha.useColors(true);
 
   const testsRoot = path.resolve(__dirname, "..");
 
-  return new Promise((c, e) => {
+  return new Promise((resolve, reject) => {
     glob("**/**.test.js", { cwd: testsRoot }, (err, files) => {
       if (err) {
-        return e(err);
+        return reject(err);
       }
 
-      // Add files to the test suite
       files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
 
       try {
-        // Run the mocha test
         mocha.run(failures => {
           if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
+            reject(new Error(`${failures} tests failed.`));
           } else {
-            c();
+            resolve();
           }
         });
       } catch (err) {
         console.error(err);
-        e(err);
+        reject(err);
       }
     });
   });
