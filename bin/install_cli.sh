88fb6e6bfbd16205e0581e7d9821f1810ed65c05
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2026-04-22 12:04:28 +0200

    feat: update install_cli.sh

--- bin/install_cli.sh
+++ bin/install_cli.sh
@@ -4,6 +4,8 @@
 set -e
 
 REPO='MichaelCurrin/auto-commit-msg'
+REPO_URL="https://github.com/$REPO/releases/latest/download"
+
 BINARIES=("acm" "gacm" "auto_commit_msg_generate")
 INSTALL_DIR="$HOME/.local/bin"
 
@@ -13,39 +15,32 @@ if [[ "$OS_NAME" == "darwin" ]]; then
 elif [[ "$OS_NAME" == "linux" ]]; then
   OS="linux"
 else
-  echo "Error: Unsupported OS ($OS_NAME)"
+  echo "Error: Unsupported OS '$OS_NAME'"
   exit 1
 fi
 
-TAG=$(
-  curl -s https://api.github.com/repos/$REPO/releases |
-    grep '"tag_name":' | head -n 1 | cut -d '"' -f 4
-)
-echo "Found latest tag: '$TAG'."
-REPO_URL="https://github.com/$REPO/releases/download/$TAG"
-
 TEMP_DIR=$(mktemp -d)
 trap 'rm -rf "$TEMP_DIR"' EXIT
-mkdir -p "$INSTALL_DIR"
+
+echo "Install directory: '$INSTALL_DIR'."
 
 for BIN in "${BINARIES[@]}"; do
   FILENAME="${BIN}-${OS}"
-  DEST_PATH="${INSTALL_DIR}/${BIN}"
 
-  echo "Installing $BIN for $OS."
   DOWNLOAD_URL="${REPO_URL}/${FILENAME}"
 
+  echo "Downloading '$FILENAME' as '$BIN'."
   if ! HTTP_STATUS=$(curl -LsS -o "${TEMP_DIR}/${BIN}" -w "%{http_code}" "$DOWNLOAD_URL"); then
     echo "Error: Failed to download '$BIN' from '$DOWNLOAD_URL'"
     exit 1
   fi
 
-  if [[ ! "$HTTP_STATUS" =~ ^2 ]]; then
-    echo "Error: Download failed for '$BIN' from '$DOWNLOAD_URL' (HTTP $HTTP_STATUS)"
+  if [[ "$HTTP_STATUS" != 200 ]]; then
+    echo "Error: HTTP $HTTP_STATUS for '$DOWNLOAD_URL'"
     exit 1
   fi
-
-  install "${TEMP_DIR}/${BIN}" "$DEST_PATH"
 done
 
-echo "Successfully installed '${BINARIES[*]}' to '$INSTALL_DIR'"
+install -d "${TEMP_DIR}/*" "$INSTALL_DIR"
+
+echo 'Successfully installed.'
